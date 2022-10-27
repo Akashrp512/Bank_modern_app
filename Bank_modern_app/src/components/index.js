266ca2b2e60573053b53fa1366bee6e4bf06e834
Akash RP <akashrp512@gmail.com>
2022-10-27 21:19:50 +0530

    first commit

---
+++ Bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing';
+import Button from './Button';
+import Business from './Business';
+import Clients from './Clients';
+import CardDeal from './CardDeal';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Navbar from './Navbar';
+
+export{
+    Navbar,
+    Billing,
+    Business,
+    Button,
+    Clients,
+    CardDeal,
+    CTA,
+    Footer,
+    FeedbackCard,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials,
+}
